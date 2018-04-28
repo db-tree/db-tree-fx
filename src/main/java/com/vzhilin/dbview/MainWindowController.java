a59569cbec93343fe2d8db28b6d620524e473a00
vzhilin <zhilin.vladimir.nic@gmail.com>
2018-04-28 17:55:23 +0300

    Доработки

--- src/main/java/com/vzhilin/dbview/MainWindowController.java
+++ src/main/java/com/vzhilin/dbview/MainWindowController.java
@@ -12,6 +12,7 @@ import com.vzhilin.dbview.tree.ToOneNode;
 import javafx.beans.property.Property;
 import javafx.beans.property.StringProperty;
 import javafx.beans.value.ChangeListener;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -131,18 +132,16 @@ public class MainWindowController {
         Parent root = loader.load();
         SettingsController controller = loader.getController();
         controller.setMainWinController(this);
-
         Stage stage = new Stage();
         stage.initOwner(ownerWindow);
         stage.initModality(Modality.WINDOW_MODAL);
         setIcon(stage);
         controller.setSettings(settings);
         Scene scene = new Scene(root, 800, 800);
-        scene.getStylesheets().add
-                (getClass().getResource("/styles/connection-settings.css").toExternalForm());
+        scene.getStylesheets().add(getClass().getResource("/styles/connection-settings.css").toExternalForm());
+        scene.getStylesheets().add(getClass().getResource("/styles/autocomplete.css").toExternalForm());
         stage.setTitle("Settings");
         stage.setScene(scene);
-
         stage.show();
     }
 
