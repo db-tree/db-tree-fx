3fe997e41b0f5893d110a2b2216588fb85279c93
Vladimir Zhilin <zhilin.vladimir.nic@gmail.com>
2019-11-13 10:08:36 +0300

    mac os installer script

--- deploy/build_dmg.sh
+++ deploy/build_dmg.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
-JAVA_HOME=~/opt/jdk/jdk-14.jdk/Contents/Home
-JAVAFX_JMODS=~/opt/openjfx/javafx-jmods-11.0.2
+JAVA_HOME=~/opt/jdk-13.0.1.jdk/Contents/Home
+JPACKAGE_HOME=~/opt/jdk/jdk-14.jdk/Contents/Home
+JAVAFX_JMODS=~/opt/javafx-jmods-13.0.1
 
-$JAVA_HOME/bin/jpackage --module-path $JAVAFX_JMODS --add-modules javafx.base,javafx.controls,javafx.fxml,javafx.graphics,java.sql,java.management,java.security.jgss \
---package-type dmg \
---icon src/main/resources/icons/icon.icns \
---mac-package-identifier me.vzhilin.dbtree \
---mac-package-name DBTree \
--d installer -i shade -n DBTree --main-class me.vzhilin.dbtree.MainWindowApp --main-jar db-tree-view-fx.jar
+if [ ! -d "target/jre" ]
+then
+	$JAVA_HOME/bin/jlink \
+	--module-path $JAVAFX_JMODS \
+	--add-modules javafx.base,javafx.controls,javafx.fxml,javafx.graphics,java.sql,java.management,java.security.jgss \
+	--output target/jre
+
+fi
+
+$JPACKAGE_HOME/bin/jpackage \
+	--copyright DBTree \
+	--runtime-image target/jre \
+	--icon src/main/resources/icons/icon.icns \
+	--vendor "Vladimir Zhilin" \
+	--app-version 0.0.1-SNAPSHOT \
+	--package-type dmg \
+	--mac-package-identifier me.vzhilin.dbtree \
+	--mac-package-name DBTree \
+	-d installer -i target/shade -n DBTree --main-class me.vzhilin.dbtree.MainWindowApp --main-jar db-tree-view-fx.jar
