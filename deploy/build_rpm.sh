7a32c092dc4dc20a600b82298dab24f7851462b2
vzhilin <vzhilin@localhost.localdomain>
2019-11-13 19:38:11 +0300

    rpm build script

---
+++ deploy/build_rpm.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+JAVA_HOME=~/opt/jdk-13.0.1/
+JPACKAGE_HOME=~/opt/openjdk-14-jpackage+1-70_linux-x64_bin/jdk-14
+JAVAFX_JMODS=~/opt/javafx-jmods-13.0.1
+
+if [ ! -d "target/jre" ]
+then
+	$JAVA_HOME/bin/jlink \
+	--module-path $JAVAFX_JMODS \
+	--add-modules javafx.base,javafx.controls,javafx.fxml,javafx.graphics,java.sql,java.management,java.security.jgss \
+	--output target/jre
+fi
+
+$JPACKAGE_HOME/bin/jpackage \
+	--copyright DBTree \
+	--runtime-image target/jre \
+	--icon src/main/resources/icons/icon.png \
+	--vendor "Vladimir Zhilin" \
+	--app-version 0.0.1a \
+	--type rpm \
+	--linux-package-name DBTree \
+	--linux-app-category Database \
+	--verbose \
+	--linux-shortcut \
+	-d installer -i target/shade -n DBTree --main-class me.vzhilin.dbtree.MainWindowApp --main-jar db-tree-view-fx.jar
