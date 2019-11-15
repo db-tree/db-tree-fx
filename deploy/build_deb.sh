22f53501d3358a2c1c303c3c592c99868dd3fbfa
vzhilin <zhilin.vladimir.nic@gmail.com>
2019-11-15 08:41:58 +0300

    deb build script

---
+++ deploy/build_deb.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+JAVA_HOME=~/opt/jdk-13.0.1/
+JPACKAGE_HOME=~/opt/jdk-14
+JAVAFX_JMODS=~/opt/javafx-jmods-13.0.1
+
+if [ ! -d "target/jre" ]
+then
+	$JAVA_HOME/bin/jlink \
+	--module-path $JAVAFX_JMODS \
+	--add-modules javafx.base,javafx.controls,javafx.fxml,javafx.graphics,java.sql,java.management,java.security.jgss,jdk.security.auth \
+	--output target/jre
+fi
+
+$JPACKAGE_HOME/bin/jpackage \
+	--copyright DBTree \
+	--runtime-image target/jre \
+	--icon src/main/resources/icons/icon.png \
+	--vendor "Vladimir Zhilin" \
+	--linux-deb-maintainer "zhilin.vladimir.nic@gmail.com" \
+	--app-version 0.0.1a \
+	--type deb \
+	--linux-package-name db-tree \
+	--linux-app-category Database \
+	--linux-shortcut \
+	-d installer -i target/shade -n db-tree --main-class me.vzhilin.dbtree.MainWindowApp --main-jar db-tree-view-fx.jar
